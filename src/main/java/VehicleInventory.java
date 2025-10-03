cbec6439ab00e4114dddbb2e047b4337207368aa
Romell Walker <walkerromell@gmail.com>
2025-10-03 17:00:54 -0400

    Started vehicle array

---
+++ src/main/java/VehicleInventory.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class VehicleInventory {
+
+
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        VehicleInventory[] inventory = new VehicleInventory[20];
+        int count = 0;
+inventory[count++]= new VehicleInventory(101121, "Ford Explorer", "Red",32775, 12250.00f);
+
+    }
+}
