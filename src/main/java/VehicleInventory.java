a39b9946f3864b8d26e269eccda24cb6af4655c9
Romell Walker <walkerromell@gmail.com>
2025-10-07 10:48:14 -0400

    fix Vehicle issue

--- src/main/java/VehicleInventory.java
+++ src/main/java/VehicleInventory.java
@@ -5,9 +5,9 @@ public class VehicleInventory {
 
     public static void main(String[] args) {
         Scanner scanner= new Scanner(System.in);
-        VehicleInventory[] inventory = new VehicleInventory[20];
+        Vehicle[] inventory = new Vehicle[20];
         int count = 0;
-inventory[count++]= new VehicleInventory(101121, "Ford Explorer", "Red",32775, 12250.00f);
+inventory[count++]= new Vehicle(101121L, "Ford Explorer", "Red",320775, 12250.00f);
 
     }
 }
